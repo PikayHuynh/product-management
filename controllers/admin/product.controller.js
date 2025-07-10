3ed814dbfc76034d731c708aeb110492883c15cf
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-07-10 21:53:49 +0700

    pagination

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -23,12 +23,37 @@ module.exports.index = async (req, res) => {
         find.title = objectSearch.regex;
     }
 
-    const products = await Product.find(find);
+
+    // pagination
+    let objectPagination = {
+        currentPage: 1,
+        limitItem: 4
+    };
+
+    if(req.query.page) {
+        if(!isNaN(parseInt(req.query.page))) {
+            objectPagination.currentPage = parseInt(req.query.page);
+        } else {
+            objectPagination.currentPage = 1;
+        }
+    }
+
+    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+    
+    const countProducts = await Product.countDocuments(find);
+    const totalPage = Math.ceil(countProducts / objectPagination.limitItem);
+    
+    objectPagination.totalPage = totalPage;
+    
+    // End pagination
+
+    const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
     
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: objectSearch.keyword
+        keyword: objectSearch.keyword,
+        pagination: objectPagination
     });
 }
