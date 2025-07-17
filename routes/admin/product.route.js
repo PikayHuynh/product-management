dfeebfdee1f34534a7fde32ac3aa261be273ad1c
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-07-18 00:47:55 +0700

    edit item product

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -17,5 +17,7 @@ router.patch("/restore/:id", controller.restoreItem);
 router.patch("/restore-multi", controller.restoreMulti);
 router.get("/create", controller.create);
 router.post("/create", upload.single("thumbnail"), validate.createPost, controller.createPost);
+router.get("/edit/:id", controller.edit);
+router.patch("/edit/:id", upload.single("thumbnail"), validate.createPost, controller.editPatch)
 
 module.exports = router;
