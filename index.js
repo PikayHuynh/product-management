072932a03f5ef3d216b32a68372c4e86b8fb3fba
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-07-22 21:23:08 +0700

    tinymce

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const methodOverride = require("method-override");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -27,8 +28,14 @@ app.set("view engine", "pug");
 
 // flash
 app.use(cookieParser("PIKADGHJFJK"));
-app.use(session({ cookie: { maxAge: 60000 }}));
+app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
+
+// TinyMCE
+app.use(
+  "/tinymce",
+  express.static(path.join(__dirname, "node_modules", "tinymce"))
+);
 
 // App locals variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
