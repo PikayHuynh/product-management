a0a472569da75ab2bfd44417af947fccfb6a149d
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-07-25 00:34:59 +0700

    edit role

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -22,10 +22,46 @@ module.exports.create = async (req, res) => {
   });
 };
 
-//[GET] /admin/roles/create
+//[POST] /admin/roles/create
 module.exports.createPost = async (req, res) => {
   const role = new Role(req.body);
   await role.save();
   req.flash("success", "Đã thêm mới nhóm quyền thành công");
   res.redirect(`$${systemConfig.prefixAdmin}/roles`);
+};
+
+//[GET] /admin/roles/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    let find = {
+      _id: id,
+      deleted: false
+    };
+
+    const data = await Role.findOne(find);
+
+    res.render("admin/pages/roles/edit", {
+      pageTitle: "Sửa nhóm quyền",
+      data: data
+    });
+  } catch(error) {
+    req.flash("error", "Không tìm thấy nhóm quyền");
+    res.redirect(`${systemConfig.prefixAdmin}/roles`);
+  }
+};
+
+//[PATCH] /admin/roles/edit/:id
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Role.updateOne({ _id: id }, req.body);
+
+    req.flash("success", "Cập nhập nhóm quyền thành công");
+  } catch(error) {
+    req.flash("error", "Cập nhập nhóm quyền thất bại");
+  }
+  const backURL = req.get("Referer");
+  res.redirect(backURL);
 };
