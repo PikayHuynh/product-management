7d9681f762a765696de9d55b5f89b9a90d6778b3
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-07-25 00:13:14 +0700

    role create

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,8 @@ const productRoutes = require("./product.route");
 
 const productCategoryRoutes = require("./product-category.route");
 
+const roleRoutes = require("./role.route");
+
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin;
 
@@ -14,4 +16,5 @@ module.exports = (app) => {
 
   app.use(PATH_ADMIN + "/products-category", productCategoryRoutes);
 
+  app.use(PATH_ADMIN + "/roles", roleRoutes);
 };
