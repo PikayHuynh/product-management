7b2ba34c577a79d58fcb74aa24bdd67637608323
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-07-28 12:18:50 +0700

    auth logout

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -46,3 +46,10 @@ module.exports.loginPost = async (req, res) => {
   res.cookie("token", user.token);
   res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
 };
+
+//[GET] /admin/auth/logout
+module.exports.logout = async (req, res) => {
+  // Xóa token
+  res.clearCookie("token");
+  res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+};
