bd8276ad4e02904ff92d9a4c26d669ffc327740a
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-07-30 21:06:26 +0700

    logs history
    Phân quyền trang

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -8,6 +8,8 @@ const ms = require("ms");
 
 const Account = require("../../models/account.model");
 
+const Role = require("../../models/role.model");
+
 module.exports.requireAuth = async (req, res, next) => {
   const token = req.cookies.token;
   const refreshToken = req.cookies.refreshToken; 
@@ -19,6 +21,19 @@ module.exports.requireAuth = async (req, res, next) => {
   try {
     const decode = authTokenHelper.verifyAccessToken(token);
     req.authenticatedAccount = decode;
+
+    const user = await Account.findOne({ _id: decode.id }).select("-password");
+      
+    if(!user) {
+      res.clearCookie("token");
+      res.clearCookie("refreshToken");
+      return res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+    } 
+
+    const role = await Role.findOne({ _id: user.role_id }).select("title permissions");
+
+    res.locals.user = user;
+    res.locals.role = role;
     next();
   } catch(error) {
 
@@ -29,13 +44,15 @@ module.exports.requireAuth = async (req, res, next) => {
     try{
       const decodeRefresh = authTokenHelper.verifyRefreshToken(refreshToken);
 
-      const user = await Account.findOne({ _id: decodeRefresh.id });
+      const user = await Account.findOne({ _id: decodeRefresh.id }).select("-password");
       
       if(!user) {
         res.clearCookie("token");
         res.clearCookie("refreshToken");
         return res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
       } 
+
+      const role = await Role.findOne({ _id: user.role_id }).select("title permissions");
 
       const newAccessToken = authTokenHelper.generateAccessToken({
         id: user._id,
@@ -49,6 +66,10 @@ module.exports.requireAuth = async (req, res, next) => {
       });
 
       req.authenticatedAccount = decodeRefresh;
+
+      res.locals.user = user;
+      res.locals.role = role;
+
       next();
     } catch(error) {
       res.clearCookie("token");
