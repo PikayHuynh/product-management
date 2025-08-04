7ad48ab5f34c25714475c1d6b47d6898f851685f
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-08-04 20:49:19 +0700

    featured view

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product.model");
 
+const productsHelper = require("../../helpers/products");
+
 //[GET] /products
 
 module.exports.index = async (req, res) => {
@@ -12,10 +14,7 @@ module.exports.index = async (req, res) => {
   //     item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0); //Thêm mới object
   // });
 
-  const newProducts = products.map((item) => {
-    item.priceNew = ((item.price * (100 - item.discountPercentage)) / 100).toFixed(0);
-    return item;
-  });
+  const newProducts = productsHelper.priceNewProducts(products);
 
   res.render("client/pages/products/index", {
     pageTitle: "Danh sách sản phẩm",
