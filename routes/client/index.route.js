ae4393ed23effcf3a2189520827e17b256f554da
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-08-09 15:28:36 +0700

    order

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,6 +2,7 @@ const productRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
 
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
@@ -19,6 +20,8 @@ module.exports = (app) => {
   app.use("/search", searchRoutes);
 
   app.use("/cart", cartRoutes);
+
+  app.use("/checkout", checkoutRoutes);
 
   
 };
