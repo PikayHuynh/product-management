4f5f408d26e1d90be94d6c3b126295b8c5f8abf7
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-08-09 14:17:03 +0700

    update product cart

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -95,3 +95,25 @@ module.exports.delete = async (req, res) => {
   const backURL = req.get("Referer");
   res.redirect(backURL);
 };
+
+
+//[GET] /cart/update/:productId/:quantity
+module.exports.update = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  const quantity = req.params.quantity;
+
+  await Cart.updateOne({
+    _id: cartId,
+    "products.product_id": productId
+  }, {
+    $set: {
+      "products.$.quantity": quantity
+    }
+  });
+
+  req.flash("success", "Đã cập nhật thành công sản phẩm trong giỏ hàng!");
+
+  const backURL = req.get("Referer");
+  res.redirect(backURL);
+};
