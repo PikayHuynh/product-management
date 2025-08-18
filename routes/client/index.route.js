8b6ee8648d829b6acd3b7aa630612446b7c1b573
Pikay2305 <151825590+PikayHuynh@users.noreply.github.com>
2025-08-18 20:12:06 +0700

    chat

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,6 +4,7 @@ const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
 const userRoutes = require("./user.route");
+const chatRoutes = require("./chat.route");
 
 
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
@@ -29,4 +30,7 @@ module.exports = (app) => {
   app.use("/checkout", checkoutRoutes);
 
   app.use("/user", userRoutes);
+
+  app.use("/chat", chatRoutes);
+  
 };
